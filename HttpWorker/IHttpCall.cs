0c972b69a997709beaac4d9ef037580e04c86ef6
vpapenko <victor.papenko@kentor.se>
2019-10-22 19:45:59 +0300

    Rewrite of HttpWorker to process calls in parallel.

--- HttpWorker/IHttpCall.cs
+++ HttpWorker/IHttpCall.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace HttpWorker
 {
-    public interface IHttpCall
+    internal interface IHttpCall<TResult>
+        : IHttpCall
     {
+        Task<TResult> Task { get; }
+    }
+
+    internal interface IHttpCall
+    {
+        void SetResult(HttpStatusCode statusCode, string responseString);
         HttpCallTypeEnum HttpType { get; set; }
         Uri Uri { get; set; }
         HttpContent Content { get; set; }
-        void SetResult(HttpStatusCode statusCode, string responseString);
     }
 }
